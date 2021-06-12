5f30c42a112d8b25400eb874ae98a07126493552
OzKrTec <ossierrae@sena.edu.co>
2021-06-11 22:19:11 -0500

    table

--- js/tabla.js
+++ js/tabla.js
@@ -8,10 +8,33 @@ var app = new Vue({
             { nombre: "Patilla", precio: 5700, cant: 3 },
         ],
     },
-    methods: {},
+    methods: {
+        eliminar(index) {
+            Swal.fire({
+                title: "Estas seguro?",
+                text: "Una vez elimine el producto no podra revertir",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "SI",
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    this.arrayProd.splice(index, 1);
+                    Swal.fire("Eliminado!", "Su producto se elimino correctamente.", "success");
+                }
+            });
+        }
+    },
     computed: {
-        imprimir() {
-            alert('Hola Mundo');
+        total: function() {
+            var total = 0;
+            for (let i = 0; i < this.arrayProd.length; i++) {
+                total +=
+                    parseInt(this.arrayProd[i].precio) *
+                    parseInt(this.arrayProd[i].cant);
+            }
+            return total;
         }
     }
 })
