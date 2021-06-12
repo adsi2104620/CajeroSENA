28d98be04840fc90e092951b5e92f4e001009c06
OzKrTec <ossierrae@sena.edu.co>
2021-06-11 21:12:06 -0500

    table

--- js/tabla.js
+++ js/tabla.js
@@ -1,6 +1,11 @@
 var app = new Vue({
     el: "#app",
     data: {
-
-    }
+        arrayProd: [
+            { nombre: "Pera", precio: 1700, cant: 20 },
+            { nombre: "Piña", precio: 3700, cant: 10 },
+            { nombre: "Manzana", precio: 1500, cant: 12 },
+            { nombre: "Patilla", precio: 5700, cant: 3 },
+        ],
+    },
 })
